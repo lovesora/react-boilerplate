793b584522e5678cea01eafce40e0e14cd99d6ed
liuxin <liuxin@liuxindeMacBook-Pro.local>
2017-07-27 14:03:57 +0800

    fix css loader

--- webpack.dev.js
+++ webpack.dev.js
@@ -85,6 +85,18 @@ var config = {
                 }
             }]
         }, {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        modules: false,
+                        url: true
+                    }
+                }]
+            })
+        }, {
             test: /src[\/\\]components[\/\\].*\.(sass|scss)$/,
             use: ExtractTextPlugin.extract({
                 fallback: 'style-loader',
--- webpack.prod.js
+++ webpack.prod.js
@@ -75,6 +75,18 @@ var config = {
                 }
             }]
         }, {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [{
+                    loader: "css-loader",
+                    options: {
+                        modules: false,
+                        url: true
+                    }
+                }]
+            })
+        }, {
             test: /src[\/\\]components[\/\\].*\.(sass|scss)$/,
             use: ExtractTextPlugin.extract({
                 fallback: 'style-loader',
